9fddac23aff27da7817e03a96044278fc99251ac
Somanyu <somanyu.03samal@gmail.com>
2022-12-21 21:15:22 +0530

    🚧 auth: sign in with password and email check

--- routes/users.js
+++ routes/users.js
@@ -2,25 +2,45 @@ var express = require('express');
 var router = express.Router();
 var User = require('../models/User');
 
-router.post('/data', function(req, res, next) {
+router.post('/register', function (req, res, next) {
   // res.send('respond with a resource');
   const email = req.body.email
   const password = req.body.password
 
   // const responseData = {myEmail: email, myPassword: password}
   // console.log(responseData);
-  
+
   const userData = new User(req.body)
 
   userData.save((err) => {
-    if(err) {
-      res.status(500).send(err)
+    if (err) {
+      res.status(500).send({ error: err })
     } else {
-      res.send({ message: 'Form data saved successfully.'})
+      res.send({ message: 'Form data saved successfully.' })
     }
   })
 
-  
 });
+
+router.post('/signin', async (req, res, next) => {
+  const email = req.body.email
+  const password = req.body.password
+
+  const user = await User.findOne({ email: email });
+
+  // Check if email exists in DB.
+  if (user) {
+    // Check if password is correct
+    if (password == user.password) {
+      res.send({ message: 'Logged in successfully' })
+      console.log('Logged in successfully');
+    } else {
+      res.status(401).send({ message: 'Incorrect password.' })
+    }
+  } else {
+    res.status(401).send({ message: 'E-mail not found!' })
+  }
+
+})
 
 module.exports = router;
